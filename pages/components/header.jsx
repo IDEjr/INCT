f83e481479347f55041dc43dd58ff0410f373b29
Renato <renpoa@gmail.com>
2022-04-14 21:17:32 -0300

    teste

--- pages/components/header.jsx
+++ pages/components/header.jsx
@@ -7,18 +7,23 @@ import style from '../../styles/header.module.css';
 
 export default function Header() {
   return (
+    <>
     <div className={style.header}>
       <div className={style.logo}>
         <Image src={logo} width={218*0.8} height={139*0.8} className={style.image}/>   
       </div>
       <nav className={style.navbar}>
         <a className={style.button} href='/'>Home</a>
+        <a className={style.button} href='/instituto'>Instituto</a>
         <a className={style.button} href='/sobre'>Instituto</a>
         <a className={style.button} href='/nucleos'>Núcleos</a>
         <a className={style.button} href='/noticias'>Notícias</a>
         <a className={style.button} href='/artigos'>Artigos</a>
-        <a className={style.button_borderless} href='/contato'>Contato</a>
-      </nav>
-    </div>    
+      </nav>      
+    </div>  
+    <div className={style.line_green}></div>
+    <div className={style.line_blue}></div> 
+    <div className={style.line_yellow}></div> 
+    </>
   )
 }
