07f74acb3556a57ddced0d70c1ccc31586fcbfc2
Eduardo Faé <eduardofaeef@gmail.com>
2022-04-19 21:42:11 -0300

    Atualizacao Sobre com JSON

--- pages/artigos/item_artigo.jsx
+++ pages/artigos/item_artigo.jsx
@@ -1,8 +1,7 @@
 import style from './artigos.module.css';
 
 export default function item_Artigo(props){
-    return(
-
+    return(        
         <a href= {props.link}>
             <div className={style.item}>            
                 <span className={style.text_name_item}>{props.nome}</span>
