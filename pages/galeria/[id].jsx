fd3ec6f6b84c60b81516b40e36f38f08e8f48b73
Renato <renpoa@gmail.com>
2022-05-03 19:54:46 -0300

    Atualização do noticias

--- pages/galeria/[id].jsx
+++ pages/galeria/[id].jsx
@@ -1,19 +1,15 @@
 import React, { Component } from 'react';
 import { useRouter } from "next/router";
-import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from 'react-responsive-carousel';
 
 import Image from 'next/image';
 
 import style from './galeria.module.css'
+import style_car from "react-responsive-carousel/lib/styles/carousel.min.css";
 
 import Header from './../components/header';
 import Footer from './../components/footer';
 
-
-    
-
-  
 export default function album_fotos(){
 
     const router = useRouter();
@@ -25,7 +21,7 @@ export default function album_fotos(){
         <div className={style.box_galeria}>
             <span className={style.title}>{id}</span>
             <div className={style.carousel_ext}>
-            <Carousel>
+            <Carousel className={style_car}>
                 <div>
                     <img src='https://s1.static.brasilescola.uol.com.br/be/conteudo/images/a-quimica-oferece-conhecimentos-muito-importantes-para-desenvolvimento-nossa-sociedade-562fd9fc8296e.jpg'/>
                     
