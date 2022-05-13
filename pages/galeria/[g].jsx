27bdcddf47cb9adfab6f01d2712725f9edc347e8
Renato <renpoa@gmail.com>
2022-05-13 15:57:40 -0300

    Índices das imagens e "pontos" invisíveis, interatividade com aplicativos móveis e zoom

--- pages/galeria/[g].jsx
+++ pages/galeria/[g].jsx
@@ -2,8 +2,6 @@ import React, { Component } from 'react';
 import { useRouter } from "next/router";
 import { Carousel } from 'react-responsive-carousel';
 
-import Image from 'next/image';
-
 import style from './galeria.module.css'
 import style_car from "react-responsive-carousel/lib/styles/carousel.min.css";
 
@@ -21,7 +19,7 @@ export default function album_fotos(){
         <div className={style.box_galeria}>
             <span className={style.title}>{id}</span>
             <div className={style.carousel_ext}>
-            <Carousel className={style_car}>
+            <Carousel emulateTouch useKeyboardArrows showStatus={false} showIndicators={false}>
                 <div>
                     <img src='https://s1.static.brasilescola.uol.com.br/be/conteudo/images/a-quimica-oferece-conhecimentos-muito-importantes-para-desenvolvimento-nossa-sociedade-562fd9fc8296e.jpg'/>
                     
