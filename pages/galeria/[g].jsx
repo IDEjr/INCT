c311fe1f6d322478d8d5194de19ee68e21455e33
Renato <renpoa@gmail.com>
2022-05-16 00:06:26 -0300

    Ajuste da passagem da dados entre a galeria e a galeria anônima
    criados os diretórios utilizados pela galeria, com uma média de 4 imagens e um json como lista de imagens.
    Feita a conexão do corousel com os diretórios e imagens.  #49

--- pages/galeria/[g].jsx
+++ pages/galeria/[g].jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { useRouter } from "next/router";
 import { Carousel } from 'react-responsive-carousel';
+import Image from 'next/image';
 
 import style from './galeria.module.css'
 import style_car from "react-responsive-carousel/lib/styles/carousel.min.css";
@@ -11,43 +12,25 @@ import Footer from '../components/footer';
 export default function album_fotos(){
 
     const router = useRouter();
-    const id = router.query.teste;
+    const title = router.query.title;
+    const link = router.query.link;
+
+    const list_Images = require(`../../public/galeria/${link}/${link}.json`);
 
     return (
         <div>
-        <Header/>
-        <div className={style.box_galeria}>
-            <span className={style.title}>{id}</span>
-            <div className={style.carousel_ext}>
-            <Carousel emulateTouch useKeyboardArrows showStatus={false} showIndicators={false}>
-                <div>
-                    <img src='https://s1.static.brasilescola.uol.com.br/be/conteudo/images/a-quimica-oferece-conhecimentos-muito-importantes-para-desenvolvimento-nossa-sociedade-562fd9fc8296e.jpg'/>
-                    
-
+            <Header/>
+            <div className={style.box_galeria}>
+                <span className={style.title}>{title}</span>            
+                <div className={style.carousel_ext}>            
+                    <Carousel emulateTouch useKeyboardArrows showStatus={false} showIndicators={false}>
+                        {list_Images.map((image, index) => 
+                            <img key={index} src={`/galeria/${link}/${image}`}/>
+                        )}
+                    </Carousel>
                 </div>
-                <div>
-                    <img src='https://s1.static.brasilescola.uol.com.br/be/conteudo/images/a-quimica-oferece-conhecimentos-muito-importantes-para-desenvolvimento-nossa-sociedade-562fd9fc8296e.jpg'/>
-                    
-
-                </div>
-                <div>
-                    <img src='https://s1.static.brasilescola.uol.com.br/be/conteudo/images/a-quimica-oferece-conhecimentos-muito-importantes-para-desenvolvimento-nossa-sociedade-562fd9fc8296e.jpg'/>
-                    
-
-                </div>
-                <div>
-                    <img src='https://s1.static.brasilescola.uol.com.br/be/conteudo/images/a-quimica-oferece-conhecimentos-muito-importantes-para-desenvolvimento-nossa-sociedade-562fd9fc8296e.jpg'/>
-                    
-
-                </div>
-                <div>
-                    <img src='https://s1.static.brasilescola.uol.com.br/be/conteudo/images/a-quimica-oferece-conhecimentos-muito-importantes-para-desenvolvimento-nossa-sociedade-562fd9fc8296e.jpg'/>                   
-
-                </div>
-            </Carousel>
             </div>
+            <Footer/>            
         </div>
-        <Footer/>            
-    </div>
     ); 
 };
