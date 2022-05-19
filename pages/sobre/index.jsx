0931f9d8efb6c4982809d21f970da6e1a47b76a2
Eduardo Faé <eduardofaeef@gmail.com>
2022-05-19 01:23:06 -0300

    Acréscimo do Título no Sobre #78

--- pages/sobre/index.jsx
+++ pages/sobre/index.jsx
@@ -16,32 +16,36 @@ export default function Sobre() {
     return (
         <div className={style.background}>
             <Header/>
-            <div className={style.margin} />
 
-            <h1 className={style.title}>Missão</h1>
-            <div className={style.box}>
-                <div className={style.mission}>
-                    <p className={style.text}>O INCT de Catálise em Sistemas Moleculares e Nanoestruturados (INCT-CMN) 
+            <h1 className={style.title}>Sobre</h1>
+            <div className={style.about}>
+
+                <h1 className={style.title}>Missão</h1>
+                <div className={style.box}>
+                    <div className={style.mission}>
+                        <p className={style.text}>O INCT de Catálise em Sistemas Moleculares e Nanoestruturados (INCT-CMN) 
                         tem por finalidade a consolidação e promoção do desenvolvimento da área de 
                         catálise para as diversas atividades industriais e de pesquisa no país.</p>
+                    </div>
                 </div>
-            </div>
 
 
-            <h1 className={style.title}>Áreas de Atuação</h1>
-            <div className={style.box}>
-                <ul className={style.area_list}>                
-                    { itens.map( ({key, imagem, imagemLar, imagemAlt, titulo, texto}) => <li key={key}><Item imagem = {imagem} width = {imagemLar} height = {imagemAlt} titulo = {titulo} texto = {texto}/></li>) }                
-                </ul>
-            </div>
-
-
-            <h1 className={style.title}>Resultados Preliminares</h1>
-            <div className={style.box}>
-                <div className={style.options}>
-                    <a href='/about/resultados_preliminares.pdf' target="_blank" className={style.button} >Resultados Preliminares</a>
-                    <a href='/about/preliminary_results.pdf' target="_blank" className={style.button} >Preliminary Results</a>
+                <h1 className={style.title}>Áreas de Atuação</h1>
+                <div className={style.box}>
+                    <ul className={style.area_list}>                
+                        { itens.map( ({key, imagem, imagemLar, imagemAlt, titulo, texto}) => <li key={key}><Item imagem = {imagem} width = {imagemLar} height = {imagemAlt} titulo = {titulo} texto = {texto}/></li>) }                
+                    </ul>
                 </div>
+
+
+                <h1 className={style.title}>Resultados Preliminares</h1>
+                <div className={style.box}>
+                    <div className={style.options}>
+                        <a href='/about/resultados_preliminares.pdf' target="_blank" className={style.button}>Resultados Preliminares</a>
+                        <a href='/about/preliminary_results.pdf' target="_blank" className={style.button}>Preliminary Results</a>
+                    </div>
+                </div>
+                
             </div>
 
             <Footer/>
