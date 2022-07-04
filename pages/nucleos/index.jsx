dcdcd0afac40455e233d7f9559ee571f673e9a7b
vlcanesin <vlcanesin@gmail.com>
2022-07-03 21:26:53 -0300

    Bug nos nucleos

--- pages/nucleos/index.jsx
+++ pages/nucleos/index.jsx
@@ -24,28 +24,17 @@ function Nucleos() {
   lista_nucleos = zip([lista_nucleos, length_pesquisadores])
   lista_nucleos.sort(function(a,b){return b[1] - a[1]})
   lista_nucleos = lista_nucleos.filter(function(value, index, arr){return value[1] != 0})
-  //console.log(lista_nucleos)
+  console.log(lista_nucleos)
 
     return (
         <div className={style.background}>
-<<<<<<< HEAD
-=======
-          <link rel="preconnect" href="https://fonts.googleapis.com"/>  
-          <link rel="preconnect" href="https://fonts.gstatic.com"/>
-          <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet"/>
-
->>>>>>> 459fd0672d08661a55ad6f171b1e080e1809e706
           <Header/>
                     
           <span className={style.title}>Núcleos</span>          
 
-<<<<<<< HEAD
           <div className={style.todosNucleos}>
-          {lista_nucleos.map( ({fundo, brasaoSrc, brasaoLar, brasaoAlt, titulo, subtitulo,
-                                p1, p2, link1, nome1, desc1, link2, nome2, desc2, credito, link}, index) => 
-=======
-          {lista_nucleos.map( ([{fundo, brasaoSrc, brasaoLar, brasaoAlt, titulo, subtitulo, credito, link}, len], index) => 
->>>>>>> 459fd0672d08661a55ad6f171b1e080e1809e706
+          {lista_nucleos.map( ([{fundo, brasaoSrc, brasaoLar, brasaoAlt, titulo, subtitulo,
+                                p1, p2, link1, nome1, desc1, link2, nome2, desc2, credito, link}, _], index) => 
           <Item key = {index}
             fundo = {`/nucleos/main/${link}/${fundo}`}
             brasao = {<Image src={`/nucleos/main/${link}/${brasaoSrc}`} height={95*brasaoAlt/brasaoLar} width={95}/>}
@@ -53,13 +42,8 @@ function Nucleos() {
             default={<Page1 titulo = {titulo} subtitulo = {subtitulo}/>} 
             title = {titulo}
             link = {link}
-<<<<<<< HEAD
           />)}
           </div>
-=======
-          />
-          )}
->>>>>>> 459fd0672d08661a55ad6f171b1e080e1809e706
           
           
           <Footer/>
