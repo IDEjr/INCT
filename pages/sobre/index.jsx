b40f42ac8f260d208a98cd36eebe06a7f1886484
Renato Silveira <renpoa@gmail.com>
2022-07-20 00:00:40 -0300

    ajuste de bugs #170

--- pages/sobre/index.jsx
+++ pages/sobre/index.jsx
@@ -34,7 +34,7 @@ export default function Sobre(props) {
             <h1 className={style.title}>Missão</h1>
             <div className={style.box}>
                 <div className={style.mission}>
-                    <p className={style.text}><ReactMarkdown children={sobre.descricao}/></p>
+                    <div className={style.text}><ReactMarkdown children={sobre.descricao}/></div>
                 </div>
             </div>
 
