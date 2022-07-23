deee568598929173a95f131596483e2833c751e4
Renato Silveira <renpoa@gmail.com>
2022-07-22 22:47:40 -0300

    update responsividade do nucleos #netlify_update

--- pages/components/item_nucleo.jsx
+++ pages/components/item_nucleo.jsx
@@ -6,27 +6,13 @@ import Style from '../../styles/item_nucleos.module.css';
 
 export default function item_Nucleo(props){
 
-    let l = props.link;
-
-    let w, h;
-    const size = 152;
-    const normal_height = 121; // container - texto - margin
-
-    if(normal_height*props.w/props.h < size) {
-        w = normal_height*props.w/props.h;
-        h = normal_height;
-    } else {
-        w = size;
-        h = size*props.h/props.w;
-    }
-
     return(
         <div className={Style.item_Lista}>
             <Link href={{ pathname: `/nucleos/${props.link}`, query: { title:props.title }}}>
                 <a className={Style.a}>                    
                     <span className={Style.title_Img}>{props.title}</span>
                     <span className={Style.imagem}>
-                        <Image src={`/posts/nucleos/${props.logo}`} width={"150%"} height={"150%"}/>
+                        <img src={`/posts/nucleos/${props.logo}`} width={"100%"}/>
                     </span>
                 </a>
             </Link>
--- pages/nucleos/index.jsx
+++ pages/nucleos/index.jsx
@@ -20,18 +20,13 @@ export function getStaticProps() {
 
 export default function nucleos(props){
   
-    let list_Image = props.list_Image
-    
-    list_Image.sort((a,b) => b.list_pesquisadores.length - a.list_pesquisadores.length)
-    list_Image = list_Image.filter((x) => x.list_pesquisadores.length != 0)
-
-    return(
+     return(
         <>
           <Header/>
           <Title title="Núcleos" color="#FAC70A"/>
           <Box invert>
-            { list_Image.map( ({title, link, logo, brasaoAlt, brasaoLar}, index) => 
-                <Item key={index} title={title} logo={logo} link={link} w={brasaoLar} h={brasaoAlt}/>
+            { props.list_Image.map( ({title, link, logo}, index) => 
+                <Item key={index} title={title} logo={logo} link={link}/>
             )}
           </Box>
           <Footer/>
