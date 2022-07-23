1a1fd595706f3805ede2fee0457e314cf17a42ac
Renato Silveira <renpoa@gmail.com>
2022-07-22 21:36:03 -0300

    update nucleos #netlify_update

--- pages/components/item_nucleo.jsx
+++ pages/components/item_nucleo.jsx
@@ -23,11 +23,11 @@ export default function item_Nucleo(props){
     return(
         <div className={Style.item_Lista}>
             <Link href={{ pathname: `/nucleos/${props.link}`, query: { title:props.title }}}>
-                <a className={Style.a}>
-                    <span className={Style.imagem}>
-                        <Image src={`/posts/nucleos/${props.logo}`} width={w} height={h}/>
-                    </span>
+                <a className={Style.a}>                    
                     <span className={Style.title_Img}>{props.title}</span>
+                    <span className={Style.imagem}>
+                        <Image src={`/posts/nucleos/${props.logo}`} width={"150%"} height={"150%"}/>
+                    </span>
                 </a>
             </Link>
         </div>
